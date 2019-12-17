4ca7eea378e1d360e4982a16edb87909ab8e55de
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2019-12-17 20:58:18 +0700

    - add new tobs

--- js/app-reader.ori.js
+++ js/app-reader.ori.js
@@ -1,7 +1,7 @@
 function getScript(e,t){t=t||"";var a=document.createElement("script");a.src=e;var n=document.getElementsByTagName("head")[0],d=!1;a.onload=a.onreadystatechange=function(){d||this.readyState&&"loaded"!=this.readyState&&"complete"!=this.readyState||(d=!0,"function"==typeof t&&t(),a.onload=a.onreadystatechange=null,n.removeChild(a))},n.appendChild(a)}function getCss(e,t){t=t||"";var a=document.createElement("link");a.rel="stylesheet",a.href=e;var n=document.getElementsByTagName("head")[0],d=!1;a.onload=a.onreadystatechange=function(){d||this.readyState&&"loaded"!=this.readyState&&"complete"!=this.readyState||(d=!0,"function"==typeof t&&t(),a.onload=a.onreadystatechange=null)},n.appendChild(a)}
 getScript("../js/lazysizes.min.js");
 getScript("https://cdn.jsdelivr.net/gh/aalfiann/ajax@3.0.4/dist/ajax.min.js", function(){
-    getScript("https://cdn.jsdelivr.net/npm/text-obfuscator@1.0.2/dist/text-obfuscator.min.js", function(){
+    getScript("../js/tobs.min.js", function(){
         getScript("../js/crypto.min.js", function(){
             getScript("../js/reader.min.js",function(){
                 getScript("https://cdn.jsdelivr.net/gh/robinparisi/tingle@0.15.2/dist/tingle.min.js",function(){
--- js/main.ori.js
+++ js/main.ori.js
@@ -1,6 +1,6 @@
 function getScript(e,t){t=t||"";var a=document.createElement("script");a.src=e;var n=document.getElementsByTagName("head")[0],d=!1;a.onload=a.onreadystatechange=function(){d||this.readyState&&"loaded"!=this.readyState&&"complete"!=this.readyState||(d=!0,"function"==typeof t&&t(),a.onload=a.onreadystatechange=null,n.removeChild(a))},n.appendChild(a)}function getCss(e,t){t=t||"";var a=document.createElement("link");a.rel="stylesheet",a.href=e;var n=document.getElementsByTagName("head")[0],d=!1;a.onload=a.onreadystatechange=function(){d||this.readyState&&"loaded"!=this.readyState&&"complete"!=this.readyState||(d=!0,"function"==typeof t&&t(),a.onload=a.onreadystatechange=null)},n.appendChild(a)}
 getScript("https://cdn.jsdelivr.net/npm/native-form-validation@1.0.1/dist/formvalidation.min.js", function(){
-    getScript("https://cdn.jsdelivr.net/npm/text-obfuscator@1.0.2/dist/text-obfuscator.min.js", function(){
+    getScript("js/tobs.min.js", function(){
         getScript("js/crypto.min.js", function(){
             getScript("js/app.min.js");
         });
