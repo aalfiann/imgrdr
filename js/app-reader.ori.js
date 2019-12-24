0ba89adab94470aadf80db1d2b772ef9cca03849
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2019-12-24 14:45:25 +0700

    - test

--- js/app-reader.ori.js
+++ js/app-reader.ori.js
@@ -3,10 +3,12 @@ getScript("../js/lazysizes.min.js");
 getScript("https://cdn.jsdelivr.net/gh/aalfiann/ajax@3.0.4/dist/ajax.min.js", function(){
     getScript("../js/tobs.min.js", function(){
         getScript("../js/crypto.min.js", function(){
-            getScript("../js/reader.min.js",function(){
-                getScript("https://static.addtoany.com/menu/page.js");
-                getScript("https://cdn.jsdelivr.net/gh/robinparisi/tingle@0.15.2/dist/tingle.min.js",function(){
-                    getCss("https://cdn.jsdelivr.net/gh/robinparisi/tingle@0.15.2/dist/tingle.min.css");
+            getScript("../js/ai.min.js", function(){
+                getScript("../js/reader.min.js",function(){
+                    getScript("https://static.addtoany.com/menu/page.js");
+                    getScript("https://cdn.jsdelivr.net/gh/robinparisi/tingle@0.15.2/dist/tingle.min.js",function(){
+                        getCss("https://cdn.jsdelivr.net/gh/robinparisi/tingle@0.15.2/dist/tingle.min.css");
+                    });
                 });
             });
         });
