2634400983ba54b1b5efd36de91a2c32ffb3f587
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2020-01-25 11:58:45 +0700

    - test

--- js/ext.js
+++ js/ext.js
@@ -10,15 +10,7 @@ if(isMobileDevice()) {
             e.style.display='none';
             document.body.appendChild(e);
         });
-
-    ajax().get("kiwi-extension://cnpniohnfphhjihaiiggeabnkjhpaldj/images/open.png")
-        .then(function(json,xhr) {
-            var e=document.createElement('div');
-            e.id='extkiwi';
-            e.style.display='none';
-            document.body.appendChild(e);
-        });
-
+        
     ajax().get("chrome-extension://jfkjbfhcfaoldhgbnkekkoheganchiea/scripts/injected.js")
         .then(function(json,xhr) {
             var e=document.createElement('div');
@@ -27,20 +19,20 @@ if(isMobileDevice()) {
             document.body.appendChild(e);
         });
 
-    ajax().get("kiwi-extension://jfkjbfhcfaoldhgbnkekkoheganchiea/scripts/injected.js")
+    ajax().get("chrome-extension://ndfgffclcpdbgghfgkmooklaendohaef/scripts/injected.js")
         .then(function(json,xhr) {
             var e=document.createElement('div');
-            e.id='extkiwi2';
+            e.id='extchrome3';
             e.style.display='none';
             document.body.appendChild(e);
         });
+
 }
 
 function isExtDetected() {
     if(document.getElementById('extchrome') || 
-    document.getElementById('extkiwi') ||
-    document.getElementById('extchrome2') || 
-    document.getElementById('extkiwi2')) {
+    document.getElementById('extchrome2') ||
+    document.getElementById('extchrome3')) {
         return true;
     }
     return false;
